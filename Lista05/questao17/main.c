c4cadba804e6b00b1957994bdb7bece6757e2ec2
Gakkou0 <fhugo.625@gmail.com>
2022-05-24 10:13:20 -0300

    tradução da questão 17 da lista 5 para inglês

--- Lista05/questao17/main.c
+++ Lista05/questao17/main.c
@@ -4,87 +4,88 @@
 
 int main()
 {
-    int rand(void);
-    int tabuleiro[3][3] = { {0, 0, 0},
+
+    int board[3][3] = { {0, 0, 0},
                             {0, 0, 0},
-                            {0, 0, 0} }, i, j, linhaUser, colunaUser, linhaPc, ColunaPc, endGame = 0, jogadaPc, jogadaUser;
+                            {0, 0, 0} }, i, j, lineUser, colunUser, linePC, colunPc, endGame = 0, turnPc, turnUser;
     srand( (unsigned)time(NULL) );
 
     do {
-        jogadaPc = 0;
-        jogadaUser = 0;
+        turnPc = 0;
+        turnUser = 0;
         for (i=0; i<3; i++){
             for (j=0; j<3; j++){
-                printf("%d  ", tabuleiro[i][j]);
+                printf("%d  ", board[i][j]);
             }
             printf("\n");
         }
-        while (jogadaUser == 0){
-            printf("Qual sua proxima jogada? \n");
-            scanf("%d  %d", &linhaUser, &colunaUser);
+        while (turnUser == 0){
+            printf("Select your position:  \n");
+            scanf("%d  %d", &lineUser, &colunUser);
 
-            if (tabuleiro[linhaUser-1][colunaUser-1] != 0){
-                printf("Posicao ja selecionada \n");
+            if (board[lineUser-1][colunUser-1] != 0){
+                printf("this position has been selected \n");
             } else {
-                tabuleiro[linhaUser-1][colunaUser-1] = 1;
-                jogadaUser = 1;
+                board[lineUser-1][colunUser-1] = 1;
+                turnUser = 1;
             }
                 
         }
-        if (tabuleiro[0][0] != 0 && tabuleiro[0][1] != 0 && tabuleiro[0][2] != 0 && 
-            tabuleiro[1][0] != 0 && tabuleiro[1][1] != 0 && tabuleiro[1][2] != 0 && 
-            tabuleiro[2][0] != 0 && tabuleiro[2][1] != 0 && tabuleiro[2][2] != 0){
-                printf("Empate \n");
+        if (board[0][0] != 0 && board[0][1] != 0 && board[0][2] != 0 && 
+            board[1][0] != 0 && board[1][1] != 0 && board[1][2] != 0 && 
+            board[2][0] != 0 && board[2][1] != 0 && board[2][2] != 0){
+                printf("draw \n");
                 break;
             }
-        while (jogadaPc == 0){
-        linhaPc = rand() % 3;
-        ColunaPc = rand() % 3;
+        while (turnPc == 0){
+        linePC = rand() %  3;
+        colunPc = rand() % 3;
 
-        if (tabuleiro[linhaPc][ColunaPc]==0){
-            tabuleiro[linhaPc][ColunaPc] = -1;
-            jogadaPc = 1;
+        if (board[linePC][colunPc]==0){
+            board[linePC][colunPc] = -1;
+            turnPc = 1;
         } 
         }
-        if (tabuleiro[0][0] + tabuleiro[0][1] + tabuleiro[0][2] ==3 ||
-        tabuleiro[1][0] + tabuleiro[1][1] + tabuleiro[1][2] == 3||
-        tabuleiro[2][0] + tabuleiro[2][1] + tabuleiro[2][2] == 3){
-            printf("Voce venceu \n");
+        if (board[0][0] + board[0][1] + board[0][2] ==3 ||
+        board[1][0] + board[1][1] + board[1][2] == 3||
+        board[2][0] + board[2][1] + board[2][2] == 3){
+            printf("You win \n");
             endGame = 1;
         }
-        if (tabuleiro[0][0] + tabuleiro[1][0] + tabuleiro[2][0] ==3 ||
-        tabuleiro[0][1] + tabuleiro[1][1] + tabuleiro[2][1] == 3||
-        tabuleiro[0][2] + tabuleiro[1][2] + tabuleiro[2][2] == 3){
-            printf("Voce venceu \n");
+        if (board[0][0] + board[1][0] + board[2][0] ==3 ||
+        board[0][1] + board[1][1] + board[2][1] == 3||
+        board[0][2] + board[1][2] + board[2][2] == 3){
+            printf("You win \n");
             endGame = 1; 
         }
-         if (tabuleiro[0][0] + tabuleiro[1][1] + tabuleiro[2][2] ==3 ||
-        tabuleiro[2][0] + tabuleiro[1][1] + tabuleiro[0][2] == 3){
-            printf("Voce venceu \n");
+         if (board[0][0] + board[1][1] + board[2][2] ==3 ||
+        board[2][0] + board[1][1] + board[0][2] == 3){
+            printf("You win \n");
             endGame = 1;
         }
-        if (tabuleiro[0][0] + tabuleiro[0][1] + tabuleiro[0][2] == -3 ||
-        tabuleiro[1][0] + tabuleiro[1][1] + tabuleiro[1][2] == -3||
-        tabuleiro[2][0] + tabuleiro[2][1] + tabuleiro[2][2] == -3){
-            printf("Voce perdeu \n");
+        if (board[0][0] + board[0][1] + board[0][2] == -3 ||
+        board[1][0] + board[1][1] + board[1][2] == -3||
+        board[2][0] + board[2][1] + board[2][2] == -3){
+            printf("You lose\n");
             endGame = 1;
         }
-        if (tabuleiro[0][0] + tabuleiro[1][0] + tabuleiro[2][0] == -3 ||
-        tabuleiro[0][1] + tabuleiro[1][1] + tabuleiro[2][1] == -3||
-        tabuleiro[0][2] + tabuleiro[1][2] + tabuleiro[2][2] == -3){
-            printf("Voce perdeu \n");
+        if (board[0][0] + board[1][0] + board[2][0] == -3 ||
+        board[0][1] + board[1][1] + board[2][1] == -3||
+        board[0][2] + board[1][2] + board[2][2] == -3){
+            printf("You lose \n");
             endGame = 1; 
         }
-         if (tabuleiro[0][0] + tabuleiro[1][1] + tabuleiro[2][2] == -3 ||
-        tabuleiro[2][0] + tabuleiro[1][1] + tabuleiro[0][2] == -3){
-            printf("Voce perdeu \n");
+         if (board[0][0] + board[1][1] + board[2][2] == -3 ||
+        board[2][0] + board[1][1] + board[0][2] == -3){
+            printf("You lose \n");
             endGame = 1;
         }
+        
 
     } while (endGame == 0);
     for (i=0; i<3; i++){
             for (j=0; j<3; j++){
-                printf("%d  ", tabuleiro[i][j]);
+                printf("%d  ", board[i][j]);
             }
             printf("\n");
         }
