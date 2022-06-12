218c8f6dd9c78039574af225ba37f3d8a07081a4
Gakkou0 <fhugo.625@gmail.com>
2022-06-12 14:10:51 -0300

    adição da questão 1 da atividade (incompleta)

---
+++ AtividadeFUP/questao03/main.c
@@ -0,0 +1,17 @@
+/* "Tiro no Escuro" é um jogo bem simples. Três jogadores são vendados e para cada um deles é
+colocado um alvo em uma posição aleatória de um tabuleiro quadrado com 5 por 5 espaços. Então,
+cada jogador dá uma quantidade palpites de onde está seu alvo até acertá-lo. Cada palpite dado sem
+alvos diminui a pontuação do jogador em 1 ponto, enquanto que acertar o seu alvo aumenta em 5
+pontos, e acertar o alvo de um outro jogador aumenta em 10 pontos. Ganha o jogador que fizer mais
+pontos. Faça um programa que simule este jogo e indique qual o jogador vencedor. Utilize pelo menos
+uma struct com os dados dos jogadores e uma matriz para o tabuleiro.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+
+    
+    return 0;
+}
